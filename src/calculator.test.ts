9259cf9e5a23910ae8a3b939e36062b9fc2d6d32
Jiahao, Woo <woojiahao1234@gmail.com>
2025-03-31 00:22:49 +0800

    Use vitest instead

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,17 +1,18 @@
-import { add, subtract, divide, multiply } from "./calculator";
+import { expect, test } from 'vitest';
+import { add, divide, multiply, subtract } from './calculator';
 
-test("add 1 + 2 to equal 3", () => {
-  expect(add(1, 2)).toBe(3)
-})
+test('add 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
 
-test("subtract 1 + 2 to equal -1", () => {
-  expect(subtract(1, 2)).toBe(-1)
-})
+test('subtract 1 + 2 to equal -1', () => {
+  expect(subtract(1, 2)).toBe(-1);
+});
 
-test("divide 1 / 2 to equal 0.5", () => {
-  expect(divide(1, 2)).toBe(0.4)
-})
+test('divide 1 / 2 to equal 0.5', () => {
+  expect(divide(1, 2)).toBe(0.4);
+});
 
-test("multiply 1 * 2 to equal 2", () => {
-  expect(multiply(1, 2)).toBe(2)
-})
+test('multiply 1 * 2 to equal 2', () => {
+  expect(multiply(1, 2)).toBe(2);
+});
