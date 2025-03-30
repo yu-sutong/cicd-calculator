c0d9d8bec84ca7798b25d29eae46049bbf59d38d
Jiahao, Woo <woojiahao1234@gmail.com>
2025-03-30 19:58:17 +0800

    Add basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { HTMLInputAutoCompleteAttribute, useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { add, divide, multiply, subtract } from "./calculator";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const xRef = useRef<HTMLInputElement>(null);
+  const yRef = useRef<HTMLInputElement>(null);
+  const [x, setX] = useState<number | undefined>(0);
+  const [y, setY] = useState<number | undefined>(0);
+  const [result, setResult] = useState<number>();
+
+  const disabled = useMemo(() => x == null || y == null, [x, y]);
+
+  const operate = useCallback((operation: (a: number, b: number) => number) => () => {
+    if (x == null || y == null) {
+      return;
+    }
+    setResult(operation(x, y))
+  }, [setResult, x, y]);
+
+  const onXChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.value == null || event.currentTarget.value === "") {
+      setX(undefined);
+    } else {
+      setX(Number(event.currentTarget.value ?? "0"))
+    }
+  }, [setX])
+
+  const onYChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.value == null || event.currentTarget.value === "") {
+      setY(undefined);
+    } else {
+      setY(Number(event.currentTarget.value ?? "0"))
+    }
+  }, [setY])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="w-[40%] mx-auto my-24 flex flex-col gap-4">
+      {result != null &&
+        <p className="text-center text-3xl font-bold">
+          {result}
         </p>
+      }
+
+      <div className="flex w-full flex-row items-center gap-4">
+        <p>x is: </p>
+        <input type="number" value={x} onChange={onXChange} ref={yRef} className="flex-1 border-1 border-neutral-400 rounded-md px-4 py-2" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="flex w-full flex-row items-center gap-4">
+        <p>y is: </p>
+        <input type="number" value={y} onChange={onYChange} ref={xRef} className="flex-1 border-1 border-neutral-400 rounded-md px-4 py-2" />
+      </div>
+
+      <div className="flex flex-row w-full justify-between">
+        <button type="button" disabled={disabled} onClick={operate(add)} className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200">Add</button>
+        <button type="button" disabled={disabled} onClick={operate(subtract)} className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200">Subtract</button>
+        <button type="button" disabled={disabled} onClick={operate(divide)} className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200">Divide</button>
+        <button type="button" disabled={disabled} onClick={operate(multiply)} className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200">Multiply</button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/calculator.ts
@@ -0,0 +1,18 @@
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function subtract(x: number, y: number): number {
+  return x - y;
+}
+
+export function multiply(x: number, y: number): number {
+  return x * y;
+}
+
+export function divide(x: number, y: number): number {
+  return x / y;
+}
+
+
+
