33e798a6961e8d57d5b27057c42a2dac41504c64
Jiahao, Woo <woojiahao1234@gmail.com>
2025-03-30 22:22:29 +0800

    Add calculator unit tests

---
+++ src/calculator.test.ts
@@ -0,0 +1,17 @@
+import { add, subtract, divide, multiply } from "./calculator";
+
+test("add 1 + 2 to equal 3", () => {
+  expect(add(1, 2)).toBe(3)
+})
+
+test("subtract 1 + 2 to equal -1", () => {
+  expect(subtract(1, 2)).toBe(-1)
+})
+
+test("divide 1 / 2 to equal 0.5", () => {
+  expect(divide(1, 2)).toBe(0.4)
+})
+
+test("multiply 1 * 2 to equal 2", () => {
+  expect(multiply(1, 2)).toBe(2)
+})
