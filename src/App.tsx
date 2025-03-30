52959cfc52bb545894a8876a3d8de227053df859
Jiahao, Woo <woojiahao1234@gmail.com>
2025-03-30 20:15:33 +0800

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { HTMLInputAutoCompleteAttribute, useCallback, useEffect, useMemo, useRef, useState } from "react"
-import { add, divide, multiply, subtract } from "./calculator";
+import { useCallback, useMemo, useRef, useState } from 'react';
+import { add, divide, multiply, subtract } from './calculator';
 
 function App() {
   const xRef = useRef<HTMLInputElement>(null);
@@ -10,55 +10,108 @@ function App() {
 
   const disabled = useMemo(() => x == null || y == null, [x, y]);
 
-  const operate = useCallback((operation: (a: number, b: number) => number) => () => {
-    if (x == null || y == null) {
-      return;
-    }
-    setResult(operation(x, y))
-  }, [setResult, x, y]);
+  const operate = useCallback(
+    (operation: (a: number, b: number) => number) => () => {
+      if (x == null || y == null) {
+        return;
+      }
+      setResult(operation(x, y));
+    },
+    [setResult, x, y],
+  );
 
-  const onXChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.currentTarget.value == null || event.currentTarget.value === "") {
-      setX(undefined);
-    } else {
-      setX(Number(event.currentTarget.value ?? "0"))
-    }
-  }, [setX])
+  const onXChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (
+        event.currentTarget.value == null ||
+        event.currentTarget.value === ''
+      ) {
+        setX(undefined);
+      } else {
+        setX(Number(event.currentTarget.value ?? '0'));
+      }
+    },
+    [setX],
+  );
 
-  const onYChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.currentTarget.value == null || event.currentTarget.value === "") {
-      setY(undefined);
-    } else {
-      setY(Number(event.currentTarget.value ?? "0"))
-    }
-  }, [setY])
+  const onYChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (
+        event.currentTarget.value == null ||
+        event.currentTarget.value === ''
+      ) {
+        setY(undefined);
+      } else {
+        setY(Number(event.currentTarget.value ?? '0'));
+      }
+    },
+    [setY],
+  );
 
   return (
     <div className="w-[40%] mx-auto my-24 flex flex-col gap-4">
-      {result != null &&
-        <p className="text-center text-3xl font-bold">
-          {result}
-        </p>
-      }
+      {result != null && (
+        <p className="text-center text-3xl font-bold">{result}</p>
+      )}
 
       <div className="flex w-full flex-row items-center gap-4">
         <p>x is: </p>
-        <input type="number" value={x} onChange={onXChange} ref={yRef} className="flex-1 border-1 border-neutral-400 rounded-md px-4 py-2" />
+        <input
+          type="number"
+          value={x}
+          onChange={onXChange}
+          ref={yRef}
+          className="flex-1 border-1 border-neutral-400 rounded-md px-4 py-2"
+        />
       </div>
 
       <div className="flex w-full flex-row items-center gap-4">
         <p>y is: </p>
-        <input type="number" value={y} onChange={onYChange} ref={xRef} className="flex-1 border-1 border-neutral-400 rounded-md px-4 py-2" />
+        <input
+          type="number"
+          value={y}
+          onChange={onYChange}
+          ref={xRef}
+          className="flex-1 border-1 border-neutral-400 rounded-md px-4 py-2"
+        />
       </div>
 
       <div className="flex flex-row w-full justify-between">
-        <button type="button" disabled={disabled} onClick={operate(add)} className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200">Add</button>
-        <button type="button" disabled={disabled} onClick={operate(subtract)} className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200">Subtract</button>
-        <button type="button" disabled={disabled} onClick={operate(divide)} className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200">Divide</button>
-        <button type="button" disabled={disabled} onClick={operate(multiply)} className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200">Multiply</button>
+        <button
+          type="button"
+          disabled={disabled}
+          onClick={operate(add)}
+          className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200"
+        >
+          Add
+        </button>
+        <button
+          type="button"
+          disabled={disabled}
+          onClick={operate(subtract)}
+          className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200"
+        >
+          Subtract
+        </button>
+        <button
+          type="button"
+          disabled={disabled}
+          onClick={operate(divide)}
+          className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200"
+        >
+          Divide
+        </button>
+        <button
+          type="button"
+          disabled={disabled}
+          onClick={operate(multiply)}
+          className="px-4 py-2 rounded-md border-1 border-neutral-400 hover:cursor-pointer disabled:bg-neutral-200"
+        >
+          Multiply
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
